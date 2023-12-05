42c71bc86df172464f998cee69f3559b0c066921
Anshul Gupta <aguptapro@gmail.com>
2023-12-05 14:56:22 -0800

    update

--- utils/watsonx.py
+++ utils/watsonx.py
@@ -1,3 +1,4 @@
+import os
 from ibm_watson_machine_learning.foundation_models import Model
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
@@ -47,3 +48,4 @@ model_choice = "ibm/granite-13b-chat-v1" # options: ibm/granite-13b-chat-v1, met
 custom_instruction = "Please provide a summary of the following text. Do not add any information that is not mentioned in the text below."
 input_text = "A large language model (LLM) is a type of language model notable for its ability to achieve general-purpose language understanding and generation. LLMs acquire these abilities by using massive amounts of data to learn billions of parameters during training and consuming large computational resources during their training and operation. LLMs are artificial neural networks (mainly transformers) and are (pre-)trained using self-supervised learning and semi-supervised learning.\n As autoregressive language models, they work by taking an input text and repeatedly predicting the next token or word. Up to 2020, fine tuning was the only way a model could be adapted to be able to accomplish specific tasks. Larger sized models, such as GPT-3, however, can be prompt-engineered to achieve similar results. They are thought to acquire embodied knowledge about syntax, semantics and ontology inherent in human language corpora, but also inaccuracies and biases present in the corpora. \n Notable examples include OpenAI's GPT models (e.g., GPT-3.5 and GPT-4, used in ChatGPT), Google's PaLM (used in Bard), and Meta's LLaMa, as well as BLOOM, Ernie 3.0 Titan, and Anthropic's Claude 2."
 get_watsonxai_response(model_choice, input_text, custom_instruction, 512, 0.7, 1)
+print("testccc " + os.environ.get("IBM_API_KEY"))
