eabbb810de1a2731bdd0cfeba5a98cd906c764d2
Anshul Gupta <aguptapro@gmail.com>
2023-12-04 23:35:31 -0800

    update

--- app/watsonx_app.py
+++ app/watsonx_app.py
@@ -1,59 +1,87 @@
 import gradio as gr
 import os
+import json
 from utils import watsonx
 
 with open('app/example.txt', 'r') as file:
     example_text = file.read()
-
-examples = {'Example Text': example_text}
-
+examples = {'Forest': example_text}
 def example_lookup(text):
-  if text:
-    return examples[text]
-  return ''
+    if text:
+        return examples[text]
+    return ''
 
 example_instruction = "Please provide a summary of the following text. Do not add any information that is not mentioned in the text below."
 
 def clear_out():
-  cleared_tuple = (gr.Textbox.update(value=""), gr.Textbox.update(value=""), gr.Textbox.update(value=""))
-  return cleared_tuple
+    cleared_tuple = (gr.Textbox.update(value=""),
+                    gr.Textbox.update(value=""),
+                    gr.Textbox.update(value=""),
+                    gr.Textbox.update(value=""))
+    return cleared_tuple
 
-def summarize(input_text, instruction_text, max_new_tokens):
-    return watsonx.get_watsonxai_response(
-        input_text=input_text,
-        instruction=instruction_text,
-        max_new_tokens=max_new_tokens
+# list of LLM models to use for text summarization
+models = ['ibm/granite-13b-chat-v1', 'meta-llama/llama-2-70b-chat']
+
+# call watsonx.py>get_watsonxai_response function
+def summarize(modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p):
+    result = watsonx.get_watsonxai_response(
+        modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p
     )
+    return result
+    #return result.strip('\n')
 
+# app design
 with gr.Blocks() as demo:
-  with gr.Row():
-    gr.Markdown("# IBM watsonx.ai - Text Summarization using Granite model")
-    example_holder = gr.Textbox(visible=False, label="Input Text", value="example")
-  with gr.Row():
-    with gr.Column(scale=6):
-      custom_instruction = gr.Textbox(label="Input your prompt instruction:", value=example_instruction)
-      input_text = gr.Textbox(label="Input your text", placeholder="Insert some long text here...")
-      example = gr.Examples(examples=[[example_instruction, "Example Text"]], inputs=[custom_instruction, example_holder])
-    with gr.Column(scale=3):
-      with gr.Accordion("Advanced Generation Options", open=False):
-        max_new_tokens = gr.Slider(minimum=0, maximum=4096, step=1, value=512, label="Max Tokens")
-      summarize_btn = gr.Button("Summarize", variant='primary')
-      reset_btn = gr.Button("Reset")
-    with gr.Column(scale=3):
-      output = gr.Textbox(label="Output Text")
+    with gr.Row():
+        gr.Markdown("# IBM watsonx.ai - Text Summarization using Granite model")
+        example_holder = gr.Textbox(visible=False, label="Input Text", value="example")
+    with gr.Row():
+        modelId = gr.Dropdown(label="Choose a Model:", choices=models, value='ibm/granite-13b-chat-v1')
+    with gr.Row():
+        # input column
+        with gr.Column(scale=4):
+            custom_instruction = gr.Textbox(label="Input your prompt instruction", value=example_instruction)
+            input_text = gr.Textbox(label="Input your text", placeholder="Insert some long text here...")
+            example = gr.Examples(examples=[[example_instruction, "Forest"]], inputs=[custom_instruction, example_holder])
+        # options and actions column
+        with gr.Column(scale=4):
+            with gr.Accordion("Advanced Generation Options", open=False):
+                max_new_tokens = gr.Slider(minimum=0, maximum=4096, step=1, value=512, label="Max Tokens")
+                temperature = gr.Slider(minimum=0.01, maximum=2.0, step=0.01, value=0.7, label="Temperature")
+                top_p = gr.Slider(minimum=0, maximum=1.0, step=0.01, value=1.0, label="Top P")
+            summarize_btn = gr.Button("Summarize", variant='primary')
+            reset_btn = gr.Button("Reset")
+        # output column
+        with gr.Column(scale=4):
+          output = gr.Textbox(label="Output Text")
 
-  summarize_btn.click(
-    fn=summarize,
-    inputs=[input_text, custom_instruction, max_new_tokens],
-    outputs=output,
-    api_name="summarize")
+    summarize_btn.click(
+        fn=summarize,
+        inputs=[modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p],
+        outputs=output,
+        api_name="summarize")
 
-  reset_btn.click(
-    fn=clear_out,
-    inputs=[],
-    outputs=[input_text, output, example_holder, custom_instruction],
-    show_progress=False)
+    reset_btn.click(
+        fn=clear_out,
+        inputs=[],
+        outputs=[input_text, output, example_holder, custom_instruction],
+        show_progress=False)
 
-  example_holder.change(fn=example_lookup, inputs=example_holder, outputs=input_text, show_progress=False)
+    example_holder.change(
+        fn=example_lookup,
+        inputs=example_holder,
+        outputs=input_text,
+        show_progress=False)
+# end app design
 
+# launch app
+'''
+demo.launch(server_port=int(os.getenv('CDSW_APP_PORT')),
+           enable_queue=True,
+           show_error=True,
+           server_name='127.0.0.1',
+)
+'''
 demo.launch(share=True)
+# end app
--- utils/watsonx.py
+++ utils/watsonx.py
@@ -6,50 +6,44 @@ from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
 GenParams().get_example_values()
 
 def get_watsonxai_response (
-  input_text, instruction, max_new_tokens
+    modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p
 ):
-  # model params
-  generate_params = {
-    GenParams.DECODING_METHOD: "greedy",
-    GenParams.MIN_NEW_TOKENS: 50,
-    GenParams.MAX_NEW_TOKENS: max_new_tokens,
-    GenParams.REPETITION_PENALTY: 2
-  }
+    # model params
+    generate_params = {
+        GenParams.DECODING_METHOD: "sample",
+        GenParams.MIN_NEW_TOKENS: 0,
+        GenParams.MAX_NEW_TOKENS: max_new_tokens,
+        GenParams.REPETITION_PENALTY: 1,
+        GenParams.TEMPERATURE: temperature,
+        GenParams.TOP_P: top_p,
+        GenParams.TOP_K: 50
+    }
 
-  # model input
-  model = Model(
-    model_id = ModelTypes.GRANITE_13B_INSTRUCT,
-    params = generate_params,
-    credentials = {
-      "apikey": "aoopT5yVxVAu6YCkRXqM3ubDXM3HzJh2lDvzDuaR2VqG",
-      "url": "https://us-south.ml.cloud.ibm.com" # TODO: parameterize?
-    },
-    project_id= "0ead8ec4-d137-4f9c-8956-50b0da4a7068"
-  )
+    if modelId == 'ibm/granite-13b-chat-v1':
+        model_id_str = ModelTypes.GRANITE_13B_CHAT
+    elif modelId == 'meta-llama/llama-2-70b-chat':
+        model_id_str = ModelTypes.LLAMA_2_70B_CHAT
 
-  # model invocation
-  # generated_response = model.generate(prompt=input) # complete json output
-  generated_response = model.generate_text(prompt=instruction + "\n\n" + input_text) # only generated text
-  print(generated_response)
-  return generated_response
+    # prepare model input
+    model = Model(
+        model_id = model_id_str,
+        params = generate_params,
+        credentials = {
+            "apikey": "aoopT5yVxVAu6YCkRXqM3ubDXM3HzJh2lDvzDuaR2VqG",
+            "url": "https://us-south.ml.cloud.ibm.com"
+        },
+        project_id="0ead8ec4-d137-4f9c-8956-50b0da4a7068"
+    )
+
+    # model invocation
+    input_prompt=custom_instruction + "\n\n Input:" + input_text + "\n\n Output: \n"
+    generated_response = model.generate_text(prompt=input_prompt)
+    print(generated_response)
+    return generated_response
 
 # end of get_watsonxai_response
 
 # test sample
-# test_input = "You are an insurance claim processor. Read the following letter from an insurance customer and extract the requestor name, the requestor email, the date of the request, the policy number, the insurance plan name, and the reimbursement amount. Format the output as shown in the example.\n\nExample Input:\nInsurance Claim Letter for Reimbursement\nFrom: __Mike Hawthorne_________ Sender’s Name\n____1234 Main Street____________\n_________________________________ Sender’s Address\n____Los Angeles, CA 90045_________ City, State, ZIP Code\n____hawm1234@gmail.com_________ Sender’s email\n____September 21, 2023_____________ Date\nTo Whom It May Concern,\nTo:____UltraCo Insurance________ Recipient’s Name\n_______2093 Lakewood Drive______\n_________________________________ Recipient’s Address\n______Sayreville, NJ 08872________ City, State, ZIP Code\nMy name is __Mike Hawthorne_______ and I have an active medical insurance policy with your company. The policy number is _175032218_______. I am writing this letter to claim reimbursement for two fillings from Stanley Smith, DDS., who is an in-network provider for UltraCo Insurance.\nEnclosed with this letter are the following supporting documents for your reference and review:\nThe original bill for $540.25 and a copy of the UltraCo website page that lists Stanley Smith, DDS. as an in-network provider in the SpecialDentalPlus plan.\nAccording to section __13.1__ of the policy, I am entitled to __80__% reimbursement. Kindly acknowledge the receiving of this claim and advise whether I have to fill some form or whether you require other documents to complete the claim request.\nLooking forward to your response and cooperation.\nSincerely,\n__Mike Hawthorne_______________ Sender’s Name\n__Mike Hawthorne__________________ Sender’s Signature\n\nExample Output:\nname: Mike Hawthorne\nemail: hawm1234@gmail.com\ndate: September 28, 2023\npolicy number: 175032218\ninsurance plan name: SpecialDentalPlus\nreimbursement amount: $540.25\n\nInput:\nInsurance Claim Letter for Reimbursement\nFrom: __Susan Brown_________\nSender's Name\n____4030 Summer Street____________\n_________________________________\nSender's Address\n____Los Angeles, CA 90045_________\nCity, State, ZIP Code\n____sbrn1234@gmail.com_________\nSender's email\nTo:____UltraCo Insurance________\nRecipient's Name\n_______2093 Lakewood Drive______\n_________________________________\nRecipient's Address\n______Sayreville, NJ 08872________\nCity, State, ZIP Code\n____September 21, 2023_____________\nDate\nTo Whom It May Concern,\nMy name is __Susan Brown_______ and I have an active medical insurance policy with\nyour company. The policy number is _200032218_______. I am writing this letter to\nclaim reimbursement for $9.20 for a custom crown provided by Smiley Boutique,\nwho is an in-network provider for UltraCo Insurance.\nEnclosed with this letter are the following supporting documents for your reference and\nreview:\nI am including the previous EOB that UltraCo says under the SpecialDentalPlus plan\nthat Smiley Boutique is not a network provider. I am also including a copy of the\nUltraCo website page that lists Smiley Boutique as an in-network provider.\nAccording to section __15.2__ of the policy, I am entitled to __80__% reimbursement.\nKindly acknowledge the receiving of this claim and advise whether I have to fill some\nform or whether you require other documents to complete the claim request.\nLooking forward to your response and cooperation.\nSincerely,\n__Susan Brown_______________\nSender's Name\n__Susan Brown__________________\nSender's Signature\n\nOutput:\n"
-# get_watsonxai_response(test_input)
-
-'''
-{
-  "model_id": "ibm/granite-13b-instruct-v1",
-  "input": "${inputs}",
-  "parameters": {
-    "decoding_method": "greedy",
-    "max_new_tokens": 200,
-    "min_new_tokens": 0,
-    "stop_sequences": [],
-    "repetition_penalty": 1
-  },
-  "project_id": "0ead8ec4-d137-4f9c-8956-50b0da4a7068"
-}
-'''
+custom_instruction="Please summarize the article that is provided \n \n"
+input_text = "A large language model (LLM) is a type of language model notable for its ability to achieve general-purpose language understanding and generation. LLMs acquire these abilities by using massive amounts of data to learn billions of parameters during training and consuming large computational resources during their training and operation. LLMs are artificial neural networks (mainly transformers) and are (pre-)trained using self-supervised learning and semi-supervised learning.\n As autoregressive language models, they work by taking an input text and repeatedly predicting the next token or word. Up to 2020, fine tuning was the only way a model could be adapted to be able to accomplish specific tasks. Larger sized models, such as GPT-3, however, can be prompt-engineered to achieve similar results. They are thought to acquire embodied knowledge about syntax, semantics and ontology inherent in human language corpora, but also inaccuracies and biases present in the corpora. \n Notable examples include OpenAI's GPT models (e.g., GPT-3.5 and GPT-4, used in ChatGPT), Google's PaLM (used in Bard), and Meta's LLaMa, as well as BLOOM, Ernie 3.0 Titan, and Anthropic's Claude 2."
+get_watsonxai_response("ibm/granite-13b-chat-v1", input_text, custom_instruction, 512, 0.7, 1)
