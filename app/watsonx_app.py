b6d0b5a195198bd2ffdf239ac31431e391fc8ca5
Anshul Gupta <aguptapro@gmail.com>
2023-12-05 00:34:20 -0800

    update

--- app/watsonx_app.py
+++ app/watsonx_app.py
@@ -23,29 +23,28 @@ def clear_out():
 # list of LLM models to use for text summarization
 models = ['ibm/granite-13b-chat-v1', 'meta-llama/llama-2-70b-chat']
 
-# call watsonx.py>get_watsonxai_response function
+# call watsonx.py > get_watsonxai_response method
 def summarize(modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p):
     result = watsonx.get_watsonxai_response(
         modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p
     )
-    return result
-    #return result.strip('\n')
+    return result.strip('<|endoftext|>')
 
 # app design
 with gr.Blocks() as demo:
     with gr.Row():
-        gr.Markdown("# IBM watsonx.ai - Text Summarization using Granite model")
+        gr.Markdown("# IBM watsonx.ai - Text Summarization")
         example_holder = gr.Textbox(visible=False, label="Input Text", value="example")
     with gr.Row():
         modelId = gr.Dropdown(label="Choose a Model:", choices=models, value='ibm/granite-13b-chat-v1')
     with gr.Row():
         # input column
-        with gr.Column(scale=4):
+        with gr.Column(scale=5):
             custom_instruction = gr.Textbox(label="Input your prompt instruction", value=example_instruction)
             input_text = gr.Textbox(label="Input your text", placeholder="Insert some long text here...")
             example = gr.Examples(examples=[[example_instruction, "Forest"]], inputs=[custom_instruction, example_holder])
         # options and actions column
-        with gr.Column(scale=4):
+        with gr.Column(scale=3):
             with gr.Accordion("Advanced Generation Options", open=False):
                 max_new_tokens = gr.Slider(minimum=0, maximum=4096, step=1, value=512, label="Max Tokens")
                 temperature = gr.Slider(minimum=0.01, maximum=2.0, step=0.01, value=0.7, label="Temperature")
--- utils/watsonx.py
+++ utils/watsonx.py
@@ -2,15 +2,13 @@ from ibm_watson_machine_learning.foundation_models import Model
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
 
-# display example params
-GenParams().get_example_values()
-
+# method - get_watsonxai_response
 def get_watsonxai_response (
     modelId, input_text, custom_instruction, max_new_tokens, temperature, top_p
 ):
     # model params
     generate_params = {
-        GenParams.DECODING_METHOD: "sample",
+        GenParams.DECODING_METHOD: DecodingMethods.SAMPLE,
         GenParams.MIN_NEW_TOKENS: 0,
         GenParams.MAX_NEW_TOKENS: max_new_tokens,
         GenParams.REPETITION_PENALTY: 1,
@@ -19,9 +17,11 @@ def get_watsonxai_response (
         GenParams.TOP_K: 50
     }
 
-    if modelId == 'ibm/granite-13b-chat-v1':
+    # model choices
+    model_id_str = ""
+    if modelId == "ibm/granite-13b-chat-v1":
         model_id_str = ModelTypes.GRANITE_13B_CHAT
-    elif modelId == 'meta-llama/llama-2-70b-chat':
+    elif modelId == "meta-llama/llama-2-70b-chat":
         model_id_str = ModelTypes.LLAMA_2_70B_CHAT
 
     # prepare model input
@@ -32,18 +32,18 @@ def get_watsonxai_response (
             "apikey": "aoopT5yVxVAu6YCkRXqM3ubDXM3HzJh2lDvzDuaR2VqG",
             "url": "https://us-south.ml.cloud.ibm.com"
         },
-        project_id="0ead8ec4-d137-4f9c-8956-50b0da4a7068"
+        project_id = "0ead8ec4-d137-4f9c-8956-50b0da4a7068"
     )
 
     # model invocation
-    input_prompt=custom_instruction + "\n\n Input:" + input_text + "\n\n Output: \n"
+    input_prompt = custom_instruction + "\nInput: \n" + input_text + "\n\n Output:"
+    print("Input---\n" + input_prompt)
     generated_response = model.generate_text(prompt=input_prompt)
-    print(generated_response)
     return generated_response
-
 # end of get_watsonxai_response
 
 # test sample
-custom_instruction="Please summarize the article that is provided \n \n"
+model_choice = "ibm/granite-13b-chat-v1" # options: ibm/granite-13b-chat-v1, meta-llama/llama-2-70b-chat
+custom_instruction = "Please provide a summary of the following text. Do not add any information that is not mentioned in the text below."
 input_text = "A large language model (LLM) is a type of language model notable for its ability to achieve general-purpose language understanding and generation. LLMs acquire these abilities by using massive amounts of data to learn billions of parameters during training and consuming large computational resources during their training and operation. LLMs are artificial neural networks (mainly transformers) and are (pre-)trained using self-supervised learning and semi-supervised learning.\n As autoregressive language models, they work by taking an input text and repeatedly predicting the next token or word. Up to 2020, fine tuning was the only way a model could be adapted to be able to accomplish specific tasks. Larger sized models, such as GPT-3, however, can be prompt-engineered to achieve similar results. They are thought to acquire embodied knowledge about syntax, semantics and ontology inherent in human language corpora, but also inaccuracies and biases present in the corpora. \n Notable examples include OpenAI's GPT models (e.g., GPT-3.5 and GPT-4, used in ChatGPT), Google's PaLM (used in Bard), and Meta's LLaMa, as well as BLOOM, Ernie 3.0 Titan, and Anthropic's Claude 2."
-get_watsonxai_response("ibm/granite-13b-chat-v1", input_text, custom_instruction, 512, 0.7, 1)
+get_watsonxai_response(model_choice, input_text, custom_instruction, 512, 0.7, 1)
